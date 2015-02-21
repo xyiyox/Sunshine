f58d59b5715917e87de759e96e7939c0379eae29
xyiyox <yiyocx@gmail.com>
2015-02-21 13:26:51 -0500

    Creado WeatherContract con las constantes a ser usadas en la definición de las tablas de la BD

--- app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -15,133 +15,56 @@
  */
 package com.example.android.sunshine.data;
 
-import android.content.ContentUris;
-import android.net.Uri;
 import android.provider.BaseColumns;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import android.text.format.Time;
 
 /**
  * Defines table and column names for the weather database.
  */
 public class WeatherContract {
 
-    // The "Content authority" is a name for the entire content provider, similar to the
-    // relationship between a domain name and its website.  A convenient string to use for the
-    // content authority is the package name for the app, which is guaranteed to be unique on the
-    // device.
-    public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
-
-    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
-    // the content provider.
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-
-    // Possible paths (appended to base content URI for possible URI's)
-    // For instance, content://com.example.android.sunshine.app/weather/ is a valid path for
-    // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
-    // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
-    // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
-
-    public static final String PATH_WEATHER = "weather";
-    public static final String PATH_LOCATION = "location";
-
-    /* TODO Uncomment for
-    4b - Finishing the FetchWeatherTask
-    https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1675098569
-    // Format used for storing dates in the database.  ALso used for converting those strings
-    // back into date objects for comparison/processing.
-    
-    public static final String DATE_FORMAT = "yyyyMMdd";
-    */
-
-    /**
-     * Converts Date class to a string representation, used for easy comparison and database lookup.
-     * @param date The input date
-     * @return a DB-friendly representation of the date, using the format defined in DATE_FORMAT.
-     */
-    /* TODO Uncomment for
-    4b - Finishing the FetchWeatherTask
-    https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1675098569
-    public static String getDbDateString(Date date){
-        // Because the API returns a unix timestamp (measured in seconds),
-        // it must be converted to milliseconds in order to be converted to valid date.
-        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
-        return sdf.format(date);
+    // To make it easy to query for the exact date, we normalize all dates that go into
+    // the database to the start of the the Julian day at UTC.
+    public static long normalizeDate(long startDate) {
+        // normalize the start date to the beginning of the (UTC) day
+        Time time = new Time();
+        time.setToNow();
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
     }
-    */
 
-    /**
-     * Converts a dateText to a long Unix time representation
-     * @param dateText the input date string
-     * @return the Date object
+    /*
+        Inner class that defines the table contents of the location table
+        Students: This is where you will add the strings.  (Similar to what has been
+        done for WeatherEntry)
      */
-    /* TODO Uncomment for
-    4b - Finishing the FetchWeatherTask
-    https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1675098569
-    public static Date getDateFromDb(String dateText) {
-        SimpleDateFormat dbDateFormat = new SimpleDateFormat(DATE_FORMAT);
-        try {
-            return dbDateFormat.parse(dateText);
-        } catch ( ParseException e ) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-    */
-
     public static final class LocationEntry implements BaseColumns {
-
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
-
-        public static final String CONTENT_TYPE =
-                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
-        public static final String CONTENT_ITEM_TYPE =
-                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
-
-        // Table Name
         public static final String TABLE_NAME = "location";
 
         // The location setting string is what will be sent to openweathermap
-        // as the location query
+        // as the location query.
         public static final String COLUMN_LOCATION_SETTING = "location_setting";
 
-        // Human readable location string, provided by the API. Because for styling,
-        // "Mountain View" ir more recognizable than 94043
+        // Human readable location string, provided by the API.  Because for styling,
+        // "Mountain View" is more recognizable than 94043.
         public static final String COLUMN_CITY_NAME = "city_name";
 
         // In order to uniquely pinpoint the location on the map when we launch the
-        // map intent, we store the latitude and longitude as returned by openweathermap
+        // map intent, we store the latitude and longitude as returned by openweathermap.
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
 
-        /**
-         * TODO YOUR CODE BELOW HERE FOR QUIZ
-         * QUIZ - 4b - Adding LocationEntry with ID UriBuilder
-         * https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/e-1604969848/m-1604969849
-         **/
-
     }
 
-/*
-/* Inner class that defines the table contents of the weather table */
+    /* Inner class that defines the table contents of the weather table */
     public static final class WeatherEntry implements BaseColumns {
-
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
-
-        public static final String CONTENT_TYPE =
-                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
-        public static final String CONTENT_ITEM_TYPE =
-                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
 
         public static final String TABLE_NAME = "weather";
 
         // Column with the foreign key into the location table.
         public static final String COLUMN_LOC_KEY = "location_id";
-        // Date, stored as Text with format yyyy-MM-dd
-        public static final String COLUMN_DATETEXT = "date";
+        // Date, stored as long in milliseconds since the epoch
+        public static final String COLUMN_DATE = "date";
         // Weather id as returned by API, to identify the icon to be used
         public static final String COLUMN_WEATHER_ID = "weather_id";
 
@@ -164,36 +87,5 @@ public class WeatherContract {
 
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
-
-
-        public static Uri buildWeatherUri(long id) {
-            return ContentUris.withAppendedId(CONTENT_URI, id);
-        }
-
-        public static Uri buildWeatherLocation(String locationSetting) {
-            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
-        }
-
-        public static Uri buildWeatherLocationWithStartDate(
-                String locationSetting, String startDate) {
-            return CONTENT_URI.buildUpon().appendPath(locationSetting)
-                    .appendQueryParameter(COLUMN_DATETEXT, startDate).build();
-        }
-
-        public static Uri buildWeatherLocationWithDate(String locationSetting, String date) {
-            return CONTENT_URI.buildUpon().appendPath(locationSetting).appendPath(date).build();
-        }
-
-        public static String getLocationSettingFromUri(Uri uri) {
-            return uri.getPathSegments().get(1);
-        }
-
-        public static String getDateFromUri(Uri uri) {
-            return uri.getPathSegments().get(2);
-        }
-
-        public static String getStartDateFromUri(Uri uri) {
-            return uri.getQueryParameter(COLUMN_DATETEXT);
-        }
     }
 }
