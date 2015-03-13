83aa4dea913868564349ea326f24f5670c0c5363
Yiyo Castillo <yiyocx@gmail.com>
2015-03-13 08:16:11 -0500

    Se abre el mapa con las coordenadas de OpenWeatherMap
    Se utilizan las coordenadas que vienen desde la API para pasarles al mapa y mostrar la ubicación del lugar donde se esta viendo el clima. Tambien se elimino la opción de actualizar manualmente de los settings

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.android.sunshine;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -70,34 +67,7 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-        if (id == R.id.action_map){
-            openPreferredLocationInMap();
-            return true;
-        }
         return super.onOptionsItemSelected(item);
-    }
-
-    private void openPreferredLocationInMap(){
-        SharedPreferences sharedPrefs =
-                PreferenceManager.getDefaultSharedPreferences(this);
-        String location = sharedPrefs.getString(
-                getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default)
-        );
-
-        //Usando el esquema de URI para mostrar una ubicacion encontrada
-        // en la documentacion oficial
-        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
-                .appendQueryParameter("q", location)
-                .build();
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(geoLocation);
-        if (intent.resolveActivity(getPackageManager()) != null){
-            startActivity(intent);
-        } else {
-            Log.d(LOG_TAG, "No se pudo iniciar: " + location + ", no fue hallada");
-        }
     }
 
     @Override
